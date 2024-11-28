402708d23ca6ece0285d76d7eea27be3f4fa43ab
Thomas Andrew Zaragoza <thomasandrewzaragoza@gmail.com>
2024-11-28 17:27:12 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import AppNavbar from './components/AppNavbar';
+import SignUp from './sign-up/SignUp.tsx';
+import SignIn from './sign-in/SignIn.tsx';
+import Dashboard from './dashboard/Dashboard.tsx';
+import Members from './dashboard/components/Members.tsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <AppNavbar /> {} */}
+      <Routes>
+        <Route path="/" element={<SignIn />} />  {/* SignIn page */}
+        <Route path="/signup" element={<SignUp />} />  {/* SignUp page */}
+        <Route path="/dashboard" element={<Dashboard />} />  {/* Dashboard page */}
+        <Route path="/members" element={<Members />} />  {/* Members page */}
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/dashboard/components/Members.tsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material';
+import * as React from 'react';
+
+
+function Members() {
+    return (
+        <Box>
+            
+        </Box>
+    );
+}
+
+export default Members;
